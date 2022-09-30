a90c37ff43a03e68e3099b7d0f8fcb8f9db6a5c6
Martin <php.coder.me@gmail.com>
2022-09-29 22:52:08 -0400

    pre flight

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -10,7 +10,7 @@ export default function Topbar() {
     <div className='topbar'>
       <div className="tobbarWrapper">
         <div className="topLeft">
-          <span className='logo'>My Admin</span>
+          <span className='logo'>Node JS Demo</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
